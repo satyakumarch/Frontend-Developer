7659898459cbd00d54fec78edc95c93758dc256d
Satya Kumar Chaudhary <satyakumarchaudhary603@gmail.com>
2024-02-08 17:09:49 +0530

    Add files via upload

---
+++ ifelseif.js
@@ -0,0 +1,34 @@
+// let a=55;
+// if(a<40 && a>30 ){
+//     console.log("C grade");
+
+// }else if(a>50 && a<75){
+//     console.log(" first divisio an")
+// }else{
+//     console.log(" A grade");
+// }   
+
+
+
+let a=10;
+let b=20;
+// key ='+'
+switch('%'){
+    case'+':
+    console.log("a+b=",a+b);
+    break;
+    case'-':
+    console.log("a-b=",a-b);
+    break;
+    case'*':
+    console.log("a*b=",a*b);
+    break;
+    case'/':
+    console.log("a/b=",a/b);
+    break;
+    case'%':
+    console.log("a%b=",a%b);
+    break;
+    default:
+    console.log("nothing print");
+}
