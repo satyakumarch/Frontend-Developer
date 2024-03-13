25869e7ac75b4eaff6a0b497b8197d91e89c7e16
satyakumarch <satyakumarchaudhary603@gmail.com>
2024-03-13 14:29:36 +0530

    adding front code

--- food Website/main.js
+++ food Website/main.js
@@ -1,6 +1,14 @@
 $document.ready(function($){
     "use strict";
-WebGLSampler.registerPlugins(ScrollTrigger);
+
+    jQuery(".menu-toggle").click(function(){
+        jQuery(".main-navigation").toggleClass("toggle");
+    });
+    jQuery(".header-menu ul li a").click(function(){
+        jQuery(".main-navigation").removeClass("toggle")
+    })
+gsap.registerPlugins(ScrollTrigger);
+
 var elementFirst=document.querySelector('.site-header');
 ScrollTrigger.create({
     trigger:"body",
@@ -15,4 +23,8 @@ function myfunction(){
     elementFirst.classList.toggle('sticky_head');
 }
 
+var scene=$(".js-parallax-scene").get(0);
+var parallaxInstance=new Parallax(scene);
+
 })
+
