0695802b2e4c5e594dc9a915c94b7fc6c82b5b1d
satyakumarch <satyakumarchaudhary603@gmail.com>
2024-07-21 15:02:45 +0545

    javaScript

--- Java-Script/one.js
+++ Java-Script/one.js
@@ -1,1 +1,72 @@
-alert("hello");
+
+// let heading=document.getElementById("header");
+// console.log(heading);
+// let header=document.getElementsByClassName("heading")
+// console.log(header)
+
+// let button=document.getElementById("myId");
+// console.log(button)
+
+// let heading=document.getElementsByClassName("myclass");
+// console.log(heading);
+// console.dir(heading)
+
+// let element=document.querySelector("p");
+// console.log(element);
+
+// let div=document.querySelector("div");
+// console.dir(div);
+
+// let div=document.querySelector("div");
+// console.log(div);
+
+// let id=div.getAttribute("id");
+// console.log(id);
+
+// let name=div.getAttribute("name");
+// console.log(name);
+
+// let para=document.querySelector("p");
+// console.log(para.getAttribute("class"));
+
+// let newbtn=document.createElement("button");
+// newbtn.innerText="click me";
+
+// newbtn.style.color="white";
+// newbtn.style.backgroundColor="red";
+
+// document.querySelector("body").prepend(newbtn);
+
+// let para=document.querySelector("p");
+
+// let btn1=document.querySelector("#btn1");
+// btn1.onclick=(evt)=>{
+//     console.log(evt);
+//     console.log(evt.type);
+//     console.log(evt.clientX,evt.clientY);    
+// }
+
+// btn1.addEventListener("click",()=>{
+//     console.log("button1 was clicked");
+// });
+// btn1.addEventListener("click",()=>{
+//     console.log("button1 was clicked");
+// });
+// let div=document.querySelector("div");
+
+let modebtn = document.querySelector("#mode");
+let body = document.querySelector("body");
+let curMode = "light";
+
+modebtn.addEventListener("click", () => {
+    if (curMode === "light") {
+        curMode = "dark";
+        body.classList.add("dark");
+        body.classList.remove("light");
+    } else {
+        curMode = "light";
+        body.classList.add("light");
+        body.classList.remove("dark");
+    }
+    console.log(curMode);
+});
