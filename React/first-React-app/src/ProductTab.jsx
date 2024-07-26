fad025eaffdfcc27ddaed560c866b9cb205fda57
satyakumarch <satyakumarchaudhary603@gmail.com>
2024-07-26 18:53:46 +0545

    react

---
+++ React/first-React-app/src/ProductTab.jsx
@@ -0,0 +1,13 @@
+
+import Product from "./Product";
+function ProductTab(){
+    return(
+
+        <>
+        <Product title="phone " price={302535}/>
+        <Product title="laptop " price={524662}/>
+        <Product title="pen " price={5263425}/>
+         </>
+    );
+} 
+export default ProductTab;
---
+++ React/first-React-app/src/Title.jsx
@@ -0,0 +1,4 @@
+function Title(){
+    return <h1>i am a student of lovely professional university</h1>
+  }
+  export default Title;
